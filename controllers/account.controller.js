9ee64b213de68cd89de64bb493748103d8c092d1
biraneves <ubirajara.neves@usp.br>
2022-10-30 07:42:14 -0300

    PATCH /account refactored using a controller.

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -74,9 +74,62 @@ const deleteAccount = async (req, res, next) => {
     }
 };
 
+// Update an account entirely
+const updateAccount = async (req, res, next) => {
+    try {
+        const account = req.body;
+
+        if (!account.id || !account.name || account.balance == null)
+            throw new Error('Id, name and balance are required fields.');
+
+        const data = JSON.parse(await readFile(global.accountsFileName));
+
+        const index = data.accounts.findIndex((a) => a.id === account.id);
+
+        if (index === -1) throw new Error('Account not found.');
+
+        data.accounts[index].name = account.name;
+        data.accounts[index].balance = account.balance;
+
+        await writeFile(global.accountsFileName, JSON.stringify(data, null, 4));
+
+        global.logger.info(
+            `PUT /account - ${JSON.stringify(data.accounts[index])}`,
+        );
+        res.send(data.accounts[index]);
+    } catch (err) {
+        next(err);
+    }
+};
+
+const updateAccountBalance = async (req, res, next) => {
+    try {
+        const account = req.body;
+        const data = JSON.parse(await readFile(global.accountsFileName));
+
+        const index = data.accounts.findIndex((a) => a.id === account.id);
+
+        if (!account.id || account.balance == null)
+            throw new Error('Id and balance are required fields.');
+
+        if (index === -1) throw new Error('Account not found.');
+
+        data.accounts[index].balance = account.balance;
+
+        await writeFile(global.accountsFileName, JSON.stringify(data, null, 4));
+
+        global.logger.info(`PATCH /updateBalance - ${JSON.stringify(account)}`);
+        res.send(data.accounts[index]);
+    } catch (err) {
+        next(err);
+    }
+};
+
 export default {
     createAccount,
     readAccounts,
     searchAccountById,
     deleteAccount,
+    updateAccount,
+    updateAccountBalance,
 };
--- routes/account.routes.js
+++ routes/account.routes.js
@@ -13,55 +13,9 @@ router.get('/:id', AccountController.searchAccountById);
 
 router.delete('/:id', AccountController.deleteAccount);
 
-router.put('/', async (req, res, next) => {
-    try {
-        const account = req.body;
+router.put('/', AccountController.updateAccount);
 
-        if (!account.id || !account.name || account.balance == null)
-            throw new Error('Id, name and balance are required fields.');
-
-        const data = JSON.parse(await readFile(global.accountsFileName));
-
-        const index = data.accounts.findIndex((a) => a.id === account.id);
-
-        if (index === -1) throw new Error('Account not found.');
-
-        data.accounts[index].name = account.name;
-        data.accounts[index].balance = account.balance;
-
-        await writeFile(global.accountsFileName, JSON.stringify(data, null, 4));
-
-        global.logger.info(
-            `PUT /account - ${JSON.stringify(data.accounts[index])}`,
-        );
-        res.send(data.accounts[index]);
-    } catch (err) {
-        next(err);
-    }
-});
-
-router.patch('/updateBalance', async (req, res, next) => {
-    try {
-        const account = req.body;
-        const data = JSON.parse(await readFile(global.accountsFileName));
-
-        const index = data.accounts.findIndex((a) => a.id === account.id);
-
-        if (!account.id || account.balance == null)
-            throw new Error('Id and balance are required fields.');
-
-        if (index === -1) throw new Error('Account not found.');
-
-        data.accounts[index].balance = account.balance;
-
-        await writeFile(global.accountsFileName, JSON.stringify(data, null, 4));
-
-        global.logger.info(`PATCH /updateBalance - ${JSON.stringify(account)}`);
-        res.send(data.accounts[index]);
-    } catch (err) {
-        next(err);
-    }
-});
+router.patch('/updateBalance', AccountController.updateAccountBalance);
 
 router.use((err, req, res, _next) => {
     global.logger.error(`${req.method} ${req.baseUrl} - ${err.message}`);
