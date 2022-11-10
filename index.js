d46e246fd305a3e2e1fbd57c5cc61ad099285893
biraneves <ubirajara.neves@usp.br>
2022-11-10 12:57:25 -0300

    Authentication HTTP - Basic

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import winston from 'winston';
 import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
+import basicAuth from 'express-basic-auth';
 import { promises as fs } from 'fs';
-import { buildSchema } from 'graphql';
+// import { buildSchema } from 'graphql';
 import { graphqlHTTP } from 'express-graphql';
 
 import accountsRouter from './routes/account.routes.js';
@@ -60,7 +61,46 @@ app.use(express.json());
 app.use(cors());
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-app.use('/account', accountsRouter);
+const getRole = (username) => {
+    if (username == 'admin') return 'admin';
+    else if (username == 'biraneves') return 'role1';
+};
+
+const authorize = (...allowed) => {
+    const isAllowed = (role) => allowed.indexOf(role) > -1;
+
+    return (req, res, next) => {
+        if (req.auth.user) {
+            const role = getRole(req.auth.user);
+
+            if (isAllowed(role)) {
+                next();
+            } else {
+                res.status(401).send('Role not allowed.');
+            }
+        } else {
+            res.status(403).send('User not found.');
+        }
+    };
+};
+
+app.use(
+    basicAuth({
+        authorizer: (username, password) => {
+            const userMatches = basicAuth.safeCompare(username, 'admin');
+            const passwordMatches = basicAuth.safeCompare(password, 'admin');
+
+            const user2Matches = basicAuth.safeCompare(username, 'biraneves');
+            const password2Matches = basicAuth.safeCompare(password, '123');
+
+            return (
+                (userMatches && passwordMatches) ||
+                (user2Matches && password2Matches)
+            );
+        },
+    }),
+);
+app.use('/account', authorize('admin'), accountsRouter);
 
 // GraphQL endpoint
 // const root = {
